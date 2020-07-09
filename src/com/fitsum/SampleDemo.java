8822cac36b47603a2644a231c107c4123c940c2e
fitsum <fitsum.s11@gmail.com>
2020-07-09 15:38:37 -0400

    Boilerplate

---
+++ src/com/fitsum/SampleDemo.java
@@ -0,0 +1,7 @@
+package com.fitsum;
+
+public interface SampleDemo {
+
+    // holds method declarations to be implemented by subclasses
+    // similar to Abstract class but interface helps us to practice multiple inheritance
+}
